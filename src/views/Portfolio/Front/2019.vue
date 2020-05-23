ccc6d2fdb041ff41ac9825399ace3587b7ff52be
EunH <leh012@emforce.co.kr>
2020-05-23 10:43:03 +0900

    guest

--- src/views/Portfolio/Front/2019.vue
+++ src/views/Portfolio/Front/2019.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    2019
+    {{ user }}
   </div>
 </template>
+
+<script>
+import Cookies from 'js-cookie'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: '',
+      list: [],
+      contents: ''
+    }
+  },
+  mounted () {
+    this.user = Cookies.get('user')
+    this.getGuestBoardList()
+  },
+  methods: {
+    getGuestBoardList () {
+      this.$axios.get('/guestBoard.json').then((res) => {
+        const modifyAdd = res.data.map((item) => {
+          return {
+            ...item,
+            editType: false
+          }
+        })
+        this.list = modifyAdd
+      })
+    }
+  }
+}
+</script>

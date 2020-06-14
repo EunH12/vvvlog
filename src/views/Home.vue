cdc09ec244f0a8ce2eb63db72442130449bc3760
EunH <leh012@emforce.co.kr>
2020-06-14 21:17:59 +0900

    run

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    Home
+    자기소개 : html5, css3, Javascript
+    사용
+    vue.js git nuxt.js storybook
+    제작 사이트
+    굿초보, 굿초보모리셔스, 굿초보어드민 프론트, 회사내부 프로젝트 어드민 프론트 등
   </div>
 </template>
 
